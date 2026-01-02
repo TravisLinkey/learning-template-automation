8ad3afb94882d5835824c93f5950189010352d06
Travis Linkey <tlinkey0818@gmail.com>
2026-01-01 19:53:24 -0800

    chore: redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import rehypeKatex from 'rehype-katex';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://travislinkey.github.io',
-	base: '/', // Set to '/your-repo-name/' for GitHub Pages deployment
+	base: '/',
 	markdown: {
 		remarkPlugins: [remarkMath],
 		rehypePlugins: [
